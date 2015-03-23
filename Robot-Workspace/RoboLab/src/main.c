fb3d652c9fed8a5dd262daf671ae7d130af07d83
maxeuli <max_uli@web.de>
2015-03-23 13:51:08 +0100

    Ich teile mit:

--- Robot-Workspace/RoboLab/src/main.c
+++ Robot-Workspace/RoboLab/src/main.c
@@ -48,117 +48,109 @@ void stop() {		//Bremse
 	ecrobot_set_motor_speed(NXT_PORT_C, 0);
 }
 
+void goonknoten() {
+	go();			//Fährt auf Knoten
+	turnrev(200);
+	stop();
+}
+
 int kompset(int turndir) {		//Speichert aktelle Fahrtrichtung
 	dir = (dir + turndir)%4;
 	return 0;
 }
 
-int kompget(int turndir) {		//
-	turndir = (dir + turndir)%4;
-	stop();
-	return 0;
-}
-
-void on() {
-	ecrobot_set_light_sensor_active(NXT_PORT_S3);
-}
-
-void off() {
-	ecrobot_set_light_sensor_inactive(NXT_PORT_S3);
-}
-
-void sound(){
-	//Tetris
-		ecrobot_sound_tone(784, 416, 30);
+void sound(int l){
+		ecrobot_sound_tone(784, 416, l);
+		systick_wait_ms(400);
+		ecrobot_sound_tone(587, 208, l);
 		systick_wait_ms(200);
-		ecrobot_sound_tone(587, 208, 30);
+		ecrobot_sound_tone(622, 208, l);
 		systick_wait_ms(200);
-		ecrobot_sound_tone(622, 208, 30);
+		ecrobot_sound_tone(698, 416, l);
+		systick_wait_ms(400);
+		ecrobot_sound_tone(622, 208, l);
 		systick_wait_ms(200);
-		ecrobot_sound_tone(698, 416, 30);
+		ecrobot_sound_tone(587, 208, l);
 		systick_wait_ms(200);
-		ecrobot_sound_tone(622, 208, 30);
+		ecrobot_sound_tone(523, 416, l);
+		systick_wait_ms(400);
+		ecrobot_sound_tone(523, 208, l);
 		systick_wait_ms(200);
-		ecrobot_sound_tone(587, 208, 30);
+		ecrobot_sound_tone(622, 208, l);
 		systick_wait_ms(200);
-		ecrobot_sound_tone(523, 416, 30);
+		ecrobot_sound_tone(784, 416, l);
+		systick_wait_ms(400);
+		ecrobot_sound_tone(698, 208, l);
 		systick_wait_ms(200);
-		ecrobot_sound_tone(523, 208, 30);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(622, 208, 30);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(784, 416, 30);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(698, 208, 30);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(622, 208, 30);
+		ecrobot_sound_tone(622, 208, l);
 		systick_wait_ms(200);
 
-		ecrobot_sound_tone(587, 416, 30);
+		ecrobot_sound_tone(587, 416, l);
+		systick_wait_ms(400);
+		ecrobot_sound_tone(587, 208, l);
 		systick_wait_ms(200);
-		ecrobot_sound_tone(587, 208, 30);
+		ecrobot_sound_tone(622, 208, l);
 		systick_wait_ms(200);
-		ecrobot_sound_tone(622, 208, 30);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(698, 416, 30);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(784, 416, 30);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(622, 416, 30);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(523, 416, 30);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(523, 416, 30);
-		systick_wait_ms(200);
+		ecrobot_sound_tone(698, 416, l);
+		systick_wait_ms(400);
+		ecrobot_sound_tone(784, 416, l);
+		systick_wait_ms(400);
+		ecrobot_sound_tone(622, 416, l);
+		systick_wait_ms(400);
+		ecrobot_sound_tone(523, 416, l);
+		systick_wait_ms(400);
+		ecrobot_sound_tone(523, 416, l);
+		systick_wait_ms(400);
 		ecrobot_sound_tone(523, 416, 0);			//Viertelpause, Frrequenz egal?
-		systick_wait_ms(200);
+		systick_wait_ms(400);
 
 		ecrobot_sound_tone(622, 208, 0);			//Achtelpause
 		systick_wait_ms(200);
-		ecrobot_sound_tone(698, 208, 30);
+		ecrobot_sound_tone(698, 208, l);
 		systick_wait_ms(200);
-		ecrobot_sound_tone(698, 208, 30);
+		ecrobot_sound_tone(622, 208, 0);			//Pause zwischen den f
+		systick_wait_ms(5);
+		ecrobot_sound_tone(698, 208, l);
 		systick_wait_ms(200);
-		ecrobot_sound_tone(831, 208, 30);
+		ecrobot_sound_tone(831, 208, l);
 		systick_wait_ms(200);
-		ecrobot_sound_tone(1047, 416, 30);
+		ecrobot_sound_tone(1047, 416, l);
+		systick_wait_ms(400);
+		ecrobot_sound_tone(932, 208, l);
 		systick_wait_ms(200);
-		ecrobot_sound_tone(932, 208, 30);
+		ecrobot_sound_tone(831, 208, l);
 		systick_wait_ms(200);
-		ecrobot_sound_tone(831, 208, 30);
+		ecrobot_sound_tone(784, 416, l);
+		systick_wait_ms(400);
+		ecrobot_sound_tone(784, 208, l);
 		systick_wait_ms(200);
-		ecrobot_sound_tone(784, 416, 30);
+		ecrobot_sound_tone(622, 208, l);
 		systick_wait_ms(200);
-		ecrobot_sound_tone(784, 208, 30);
+		ecrobot_sound_tone(784, 416, l);
+		systick_wait_ms(400);
+		ecrobot_sound_tone(698, 208, l);
 		systick_wait_ms(200);
-		ecrobot_sound_tone(622, 208, 30);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(784, 416, 30);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(698, 208, 30);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(622, 208, 30);
+		ecrobot_sound_tone(622, 208, l);
 		systick_wait_ms(200);
 
-		ecrobot_sound_tone(587, 416, 30);
+		ecrobot_sound_tone(587, 416, l);
+		systick_wait_ms(400);
+		ecrobot_sound_tone(587, 208, l);
 		systick_wait_ms(200);
-		ecrobot_sound_tone(587, 208, 30);
+		ecrobot_sound_tone(622, 208, l);
 		systick_wait_ms(200);
-		ecrobot_sound_tone(622, 208, 30);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(698, 416, 30);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(784, 416, 30);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(622, 416, 30);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(523, 416, 30);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(523, 416, 30);
-		systick_wait_ms(200);
+		ecrobot_sound_tone(698, 416, l);
+		systick_wait_ms(400);
+		ecrobot_sound_tone(784, 416, l);
+		systick_wait_ms(400);
+		ecrobot_sound_tone(622, 416, l);
+		systick_wait_ms(400);
+		ecrobot_sound_tone(523, 416, l);
+		systick_wait_ms(400);
+		ecrobot_sound_tone(523, 416, l);
+		systick_wait_ms(400);
 		ecrobot_sound_tone(523, 416, 0);
-		systick_wait_ms(200);
-
+		systick_wait_ms(400);
 	}
 
 void set() {	//Kalibrierung auf Schwarz und Weiß
@@ -191,7 +183,7 @@ void set() {	//Kalibrierung auf Schwarz und Weiß
 	stop();
 }
 
-int search() {				//Suche nach schwarrzer Linie
+int search() {				//Suche nach schwarzer Linie
   	int i;					//optimierbar, zB add kleineres Schwenken am Anfang -> schneller
 	turnr();				//oder spezielle Suche am Knoten, letztes turnr(zurückgehen) entfernen
   	i = 0;					//WICHTG: großflächige Suche aber lassen, zum schwarze Kanten finden
@@ -225,8 +217,7 @@ void tokenfound() {			//tokenfound = Token gefunden, okay?!
 	ecrobot_set_motor_speed(NXT_PORT_C, -50);
 	systick_wait_ms(500);
 	stop();
-	token++;				//weiterer Token gefunden
-	sound();				//Superhit des Jahrhunderts wird abgespielt
+	sound(10);				//Superhit des Jahrhunderts wird abgespielt
 	systick_wait_ms(3000);
 }
 
@@ -246,9 +237,6 @@ void drive() {				//fahren auf dem Strich
 	int sw = search();	//bei 0 ist alles weiß -> Knoten
 	if (sw > 0)
 		drive();
-	go();
-	turnrev(200);
-	stop();
 }
 
 void turn90 (int t) {		//Drehung um 90°...also fast
@@ -318,7 +306,7 @@ void knoten() {		//Startet suche nach Kanten am Koten, wandelt sie um, lässt Ri
 		case 6: ecrobot_status_monitor("Links u Rechts"); NESW(1); NESW(3); break;
 		case 7: ecrobot_status_monitor("ueberall Kanten"); NESW(0); NESW(1); NESW(3);
 	}
-	komset(1);
+	kompset(1);
 	N = N * 0x10;
 	S = S * 0x20;
 	W = W * 0x40;
@@ -330,7 +318,7 @@ void knoten() {		//Startet suche nach Kanten am Koten, wandelt sie um, lässt Ri
 	display_update();							//ohne was zu tun gehts nach rechts
 }
 
-godi(int a) {
+void godi(int a) {
 	a = dir - a;		//Berechnet aus absoluter Richtung, ob er L R usw. muss
 	if ((a*a) > 6)
 		a = a * (-1/3);
@@ -341,47 +329,28 @@ godi(int a) {
 TASK(OSEK_Main_Task) {
 	ecrobot_set_light_sensor_active(NXT_PORT_S3);
 	set();
+	int hex = -1;
+	while (hex < 4) {
+	hex = (hex + 1)%4;
 	drive();
-	knoten();
-	int hex;
+	goonknoten();
+	knoten();		//startet Kantensuche am Knoten
+
 	switch (hex) {
-	case 0x10: hex = 0; break;
-	case 0x20: hex = 1; break;
-	case 0x40: hex = 2; break;
-	case 0x80: hex = 3; break;
+	case 0x10: hex = 0; break;//Norden
+	case 0x80: hex = 1; break;//Ostern-right
+	case 0x20: hex = 2; break;//Süden-back
+	case 0x40: hex = 3; break;//Westen-left
+
 	}
 	godi(hex);		//dreht in die angegebene absolute Richtung
+	}
 
-		/*turnl();
-		int i = 1;
-		while(i) {
-			if ((ecrobot_get_motor_rev(NXT_PORT_C)) > 1080)
-				i=0;
-		}*/
-		//search();
-
-		/* Touch Control:
-		int running = 1;
-		int i=0;
-		while(running) {
-				if(ecrobot_get_touch_sensor(NXT_PORT_S1))
-					running = 0;
-				if(i>4)
-					break;
-				i++;
-				display_clear(1);
-				display_int(ecrobot_get_light_sensor(NXT_PORT_S3), 4);
-				display_update();
-				systick_wait_ms(500);
-		}
-		if ((ecrobot_get_motor_rev(NXT_PORT_B) > 980) //360°
-		systick_wait_ms(5000);
-		*/
-		ecrobot_status_monitor("My name is Horst");
-		stop();
-		systick_wait_ms(3000);
-		ecrobot_status_monitor("James Horst");
-		while(1){
+	ecrobot_status_monitor("My name is Horst");
+	stop();
+	systick_wait_ms(3000);
+	ecrobot_status_monitor("James Horst");
+	while(1){
 
 		}
 }
