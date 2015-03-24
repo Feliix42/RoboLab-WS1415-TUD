bb15acd90971bdca1ea8fb34895f36421776a4bb
Felix Wittwer <felix.wittwer@gmail.com>
2015-03-24 15:39:35 +0100

    Orchester outsourced.

--- Robot-Workspace/RoboLab/src/main.c
+++ Robot-Workspace/RoboLab/src/main.c
@@ -1,4 +1,5 @@
 #include "../h/main.h"
+
 int color;		//Mittelwert Schwarz-Weiß, zum Vergleichen
 int dir = 0;	//aktuelle Fahrtrichtung N oder Startrichtung = 0
 int N;			//Startrichtung
@@ -50,100 +51,6 @@ void stop() {		//Bremse
 void kompset(int turndir) {		//Speichert aktelle Fahrtrichtung
 	dir = (dir + turndir)%4;
 }
-
-void sound(int l){
-		ecrobot_sound_tone(784, 416, l);
-		systick_wait_ms(400);
-		ecrobot_sound_tone(587, 208, l);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(622, 208, l);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(698, 416, l);
-		systick_wait_ms(400);
-		ecrobot_sound_tone(622, 208, l);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(587, 208, l);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(523, 416, l);
-		systick_wait_ms(400);
-		ecrobot_sound_tone(523, 208, l);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(622, 208, l);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(784, 416, l);
-		systick_wait_ms(400);
-		ecrobot_sound_tone(698, 208, l);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(622, 208, l);
-		systick_wait_ms(200);
-
-		ecrobot_sound_tone(587, 416, l);
-		systick_wait_ms(400);
-		ecrobot_sound_tone(587, 208, l);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(622, 208, l);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(698, 416, l);
-		systick_wait_ms(400);
-		ecrobot_sound_tone(784, 416, l);
-		systick_wait_ms(400);
-		ecrobot_sound_tone(622, 416, l);
-		systick_wait_ms(400);
-		ecrobot_sound_tone(523, 416, l);
-		systick_wait_ms(400);
-		ecrobot_sound_tone(523, 416, l);
-		systick_wait_ms(400);
-		ecrobot_sound_tone(523, 416, 0);			//Viertelpause, Frrequenz egal?
-		systick_wait_ms(400);
-
-		ecrobot_sound_tone(622, 208, 0);			//Achtelpause
-		systick_wait_ms(200);
-		ecrobot_sound_tone(698, 208, l);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(622, 208, 0);			//Pause zwischen den f
-		systick_wait_ms(5);
-		ecrobot_sound_tone(698, 208, l);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(831, 208, l);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(1047, 416, l);
-		systick_wait_ms(400);
-		ecrobot_sound_tone(932, 208, l);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(831, 208, l);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(784, 416, l);
-		systick_wait_ms(400);
-		ecrobot_sound_tone(784, 208, l);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(622, 208, l);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(784, 416, l);
-		systick_wait_ms(400);
-		ecrobot_sound_tone(698, 208, l);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(622, 208, l);
-		systick_wait_ms(200);
-
-		ecrobot_sound_tone(587, 416, l);
-		systick_wait_ms(400);
-		ecrobot_sound_tone(587, 208, l);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(622, 208, l);
-		systick_wait_ms(200);
-		ecrobot_sound_tone(698, 416, l);
-		systick_wait_ms(400);
-		ecrobot_sound_tone(784, 416, l);
-		systick_wait_ms(400);
-		ecrobot_sound_tone(622, 416, l);
-		systick_wait_ms(400);
-		ecrobot_sound_tone(523, 416, l);
-		systick_wait_ms(400);
-		ecrobot_sound_tone(523, 416, l);
-		systick_wait_ms(400);
-		ecrobot_sound_tone(523, 416, 0);
-		systick_wait_ms(400);
-	}
 
 void set() {	//Kalibrierung auf Schwarz und Weiß
 	//hier möglichst nix ändern, da es zum Glück ordentlich funzt
