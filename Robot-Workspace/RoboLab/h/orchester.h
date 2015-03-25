c0f4b8eeb62c52f8e3f791217305f4abf0afda0b
Tim <Tim.Stelzig@gmx.de>
2015-03-25 11:17:20 +0100

    Finishing Sounds

--- Robot-Workspace/RoboLab/h/orchester.h
+++ Robot-Workspace/RoboLab/h/orchester.h
@@ -1,12 +1,17 @@
 #ifndef ORCHESTER_H_
 #define ORCHESTER_H_
 
+// DEFINES
+
+#define VOLUME 50
+
 // INCLUDES
 
 #include "ecrobot_interface.h"
 
 // FUNCTIONS
 
+void finish(int l);
 void sound(int l);
 void tetris(int l);
 
